4fedbfe6c91b9fcaf94096b8c7fee9494ad581f1
Bram Gerritsen <bgerritsen@gmail.com>
2016-02-20 10:56:05 +0100

    Drop 5.3 support. We can use traits now

--- src/StrokerForm/Renderer/AbstractRenderer.php
+++ src/StrokerForm/Renderer/AbstractRenderer.php
@@ -12,6 +12,7 @@ namespace StrokerForm\Renderer;
 
 use Zend\I18n\Translator\TranslatorAwareInterface;
 use Zend\I18n\Translator\Translator;
+use Zend\I18n\Translator\TranslatorAwareTrait;
 use Zend\I18n\Translator\TranslatorInterface;
 use Zend\Mvc\Router\RouteInterface;
 use Zend\Stdlib\AbstractOptions;
@@ -19,31 +20,12 @@ use StrokerForm\FormManager;
 
 abstract class AbstractRenderer implements RendererInterface, TranslatorAwareInterface
 {
+    use TranslatorAwareTrait;
+
     /**
      * @var \Zend\Mvc\Router\RouteInterface
      */
     protected $httpRouter;
-
-    /**
-     * Translator (optional)
-     *
-     * @var TranslatorInterface
-     */
-    protected $translator;
-
-    /**
-     * Translator text domain (optional)
-     *
-     * @var string
-     */
-    protected $translatorTextDomain = 'default';
-
-    /**
-     * Whether translator should be used
-     *
-     * @var bool
-     */
-    protected $translatorEnabled = true;
 
     /**
      * @var AbstractOptions
@@ -59,96 +41,6 @@ abstract class AbstractRenderer implements RendererInterface, TranslatorAwareInt
      * @var FormManager
      */
     protected $formManager;
-
-    /**
-     * Sets translator to use in helper
-     *
-     * @param TranslatorInterface $translator [optional] translator.
-     *                                 Default is null, which sets no translator.
-     * @param string $textDomain [optional] text domain
-     *                                 Default is null, which skips setTranslatorTextDomain
-     * @return AbstractTranslatorHelper
-     */
-    public function setTranslator(TranslatorInterface $translator = null, $textDomain = null)
-    {
-        $this->translator = $translator;
-        if (null !== $textDomain) {
-            $this->setTranslatorTextDomain($textDomain);
-        }
-
-        return $this;
-    }
-
-    /**
-     * Returns translator used in helper
-     *
-     * @return Translator|null
-     */
-    public function getTranslator()
-    {
-        if (!$this->isTranslatorEnabled()) {
-            return null;
-        }
-
-        return $this->translator;
-    }
-
-    /**
-     * Checks if the helper has a translator
-     *
-     * @return bool
-     */
-    public function hasTranslator()
-    {
-        return (bool) $this->getTranslator();
-    }
-
-    /**
-     * Sets whether translator is enabled and should be used
-     *
-     * @param bool $enabled [optional] whether translator should be used.
-     *                       Default is true.
-     * @return AbstractTranslatorHelper
-     */
-    public function setTranslatorEnabled($enabled = true)
-    {
-        $this->translatorEnabled = (bool) $enabled;
-
-        return $this;
-    }
-
-    /**
-     * Returns whether translator is enabled and should be used
-     *
-     * @return bool
-     */
-    public function isTranslatorEnabled()
-    {
-        return $this->translatorEnabled;
-    }
-
-    /**
-     * Set translation text domain
-     *
-     * @param  string                   $textDomain
-     * @return AbstractTranslatorHelper
-     */
-    public function setTranslatorTextDomain($textDomain = 'default')
-    {
-        $this->translatorTextDomain = $textDomain;
-
-        return $this;
-    }
-
-    /**
-     * Return the translation text domain
-     *
-     * @return string
-     */
-    public function getTranslatorTextDomain()
-    {
-        return $this->translatorTextDomain;
-    }
 
     /**
      * @return RouteInterface
