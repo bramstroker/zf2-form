4cf7fb257ced535841f6593dfe3145faf7bb8ba9
Cristian Bichis <cristi@imagis.ro>
2017-06-09 01:13:46 +0300

    misc

--- assets/jquery_validate/js/custom_rules.js
+++ assets/jquery_validate/js/custom_rules.js
@@ -12,33 +12,10 @@ $.validator.addMethod("in_array", function(value, element, haystack) {
     }
 
     return check;
-});
+}, $.validator.format( "In array error." ) );
 
-$.validator.addMethod(
-    "regex",
-    function(value, element, pattern) {
-        var check = false;
-        var regexp = new RegExp(pattern);
-        return this.optional(element) || regexp.test(value);
-    }
-);
-/*
-checks array notation; see effect over checkbox with hidden element
- */
-/*
-$.extend($.validator.prototype, {
-    checkForm: function() {
-        this.prepareForm();
-        for ( var i = 0, elements = (this.currentElements = this.elements()); elements[i]; i++ ) {
-            if (this.findByName( elements[i].name ).length != undefined && this.findByName( elements[i].name ).length > 1) {
-                for (var cnt = 0; cnt < this.findByName( elements[i].name ).length; cnt++) {
-                    this.check( this.findByName( elements[i].name )[cnt] );
-                }
-            } else {
-                this.check( elements[i] );
-            }
-        }
-        return this.valid();
-    }
-});
-*/
+$.validator.addMethod("regex", function(value, element, pattern) {
+    var check = false;
+    var regexp = new RegExp(pattern);
+    return this.optional(element) || regexp.test(value);
+}, $.validator.format( "Regex error." ) );
